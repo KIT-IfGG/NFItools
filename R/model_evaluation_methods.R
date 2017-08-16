930137ccbe70790cc64d62307c04aeb535d79284
kdolos <dolos@kit.edu>
2017-08-16 15:13:45 +0200

    kappa bug

--- R/model_evaluation_methods.R
+++ R/model_evaluation_methods.R
@@ -4,9 +4,9 @@ calc_kappa <- function(x, obs_values, pred_values) {
   SDMTools::Kappa(mat)
 }
 
-threshold_kappa <- function(x=0.5, obs_values = obs, pred_values = pred, ...){
+threshold_kappa <- function(x=0.5, obs_values, pred_values, ...){
   fn <- function(x) {
-    calc_kappa(x, obs_values = obs, pred_values = pred)
+    calc_kappa(x, obs_values = obs_values, pred_values = pred_values)
   }
   optimize(fn, interval=c(0,1), maximum=TRUE)
 }
